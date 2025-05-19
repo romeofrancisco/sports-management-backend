15f7ad59448ceaf0dbc0cfdaecab45e98af88f60
romeofrancisco <romeofrancisco.works@gmail.com>
2025-05-19 14:14:07 +0800

    add permissions to players and team

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import AdminPasswordChangeForm
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name', 'phone_number')
+    list_filter = ('role', 'sex', 'is_active', 'is_staff')
+    ordering = ('first_name', 'last_name')
+    change_password_form = AdminPasswordChangeForm
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'sex', 'date_of_birth', 'phone_number', 'profile')}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'sex', 'password1', 'password2', 'role'),
+        }),
+    )
     
